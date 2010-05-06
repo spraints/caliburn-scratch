c0ceaeb716c1e16592499613f49ed02c0176ea7b
Matt Burke <maburke@sep.com>
2010-05-06 12:02:41 -0400

    Added a main window.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,43 @@
-﻿using Caliburn.PresentationFramework.ApplicationModel;
+﻿using Caliburn.Core;
+using Caliburn.PresentationFramework.ApplicationModel;
+using Caliburn.StructureMap;
+using StructureMap;
+using Microsoft.Practices.ServiceLocation;
 
 namespace Scratch
 {
     public partial class App : CaliburnApplication
     {
+        protected override IServiceLocator CreateContainer()
+        {
+            var structureMap = new Container(config =>
+            {
+                config.Scan(scan =>
+                {
+                    scan.WithDefaultConventions();
+                    scan.TheCallingAssembly();
+                });
+
+                config.ForSingletonOf<MyDataModel>();
+            });
+
+            return new StructureMapAdapter(structureMap);
+        }
+
+        protected override void BeforeStart(CoreConfiguration core)
+        {
+            base.BeforeStart(core);
+            core.AfterStart(() =>
+            {
+                var binder = (DefaultBinder)Container.GetInstance<IBinder>();
+                binder.EnableBindingConventions();
+                binder.EnableMessageConventions();
+            });
+        }
+
+        protected override object CreateRootModel()
+        {
+            return Container.GetInstance<ShellViewModel>();
+        }
     }
 }
